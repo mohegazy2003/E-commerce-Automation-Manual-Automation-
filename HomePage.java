1c1540e7bfe9dafbcd508abe2f2e3f5e70bf4a05
Hager Waheed <hagerwaheed002@gmail.com>
2025-11-30 23:27:39 +0200

    ContactUS-home page

--- HomePage.java
+++ HomePage.java
@@ -1,30 +1,25 @@
-package pages;
+package Page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
-    WebDriver driver ;
+    WebDriver driver;
+
     public HomePage(WebDriver driver) {
-        this.driver =driver ;
+        this.driver=driver;
     }
-    //locter
-    private By loginSignupage = By.xpath("//a[normalize-space()='Signup / Login']");
-    private By deleteAccount = By.xpath("(//a[@style='color:brown;'])[2]");
 
+      //Locators
+     private By ClickOnContactUs= By.linkText("Contact us");
 
+    //Actions
+    public ContactUsPage ClickOnContactUs(){
+        driver.findElement(ClickOnContactUs).click();
+        return new ContactUsPage(driver);
 
-    //Acction
-
-    public SingUploginPage clickonloginSignupbutton(){
-    driver.findElement(loginSignupage).click();
-    return new SingUploginPage(driver);
-    }
-    public DeleteAccountPage clickonremoveaccont(){
-        driver.findElement(deleteAccount).click();
-        return new DeleteAccountPage(driver);
     }
 
 
+}
 
-    }
