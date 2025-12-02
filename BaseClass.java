e8eddeff7ebf77c917a44d3e298390006e02d374
janaayman-lgtm <janaayman929@gmail.com>
2025-12-02 13:33:21 +0200

    Update BaseClass.java

--- BaseClass.java
+++ BaseClass.java
@@ -1,27 +1,38 @@
 package base;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import java.time.Duration;
+import pages.HomePage;
+
 
 public class BaseClass {
-    protected WebDriver driver;
-    protected final String BASE_URL = "https://automationexercise.com/";
 
-    @BeforeMethod
+    public WebDriver driver;
+    public HomePage homePage;
+
+    @BeforeClass
     public void setup() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get(BASE_URL);
+        homePage = new HomePage(driver);
+        goHome();
     }
 
-    @AfterMethod
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
+    @BeforeMethod
+    public void goHome() {
+        driver.get("https://automationexercise.com/");
+
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+
+        driver.quit();
     }
 }
+
+
 
